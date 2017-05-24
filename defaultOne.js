3b3c9caff11ed241b1522d2ebe6dfa74d47cca73
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-05-24 10:29:00 -0400

    8th exercise in Count-to-6

--- defaultOne.js
+++ defaultOne.js
@@ -1,5 +1,5 @@
-module.exports = function midpoint(greeting = "Hello", name = "CampJS") {
-    console.log(`${greeting} ${name}!`);
+module.exports = function midpoint(lower = 0, upper = 1) {
+  return (upper - lower)/2;
 };
 
 
